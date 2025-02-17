7d001efa058bf39c70b0efdc983c3d5d05d99096
yulumstack <alahyalo@gmail.com>
2025-02-17 17:39:11 +0100

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,13 @@
+var mb = document.getElementById('menuBtn')
+var sv = document.getElementById('sidenav')
+
+sv.style.right= "-250px"
+mb.onclick = function()
+{
+  if(sv. style.right == "-250px")
+  {sv.style.right ="0";}
+  else{sv. style.right = "-250px"}
+}
+
+
+Document.write(typeof 10)
